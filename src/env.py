a7fd575b4187d469e15b8383700b47858700ec15
kovika98 <61557355+kovika98@users.noreply.github.com>
2021-05-04 12:55:33 +0200

    env render

--- src/env.py
+++ src/env.py
@@ -30,7 +30,7 @@ class DoubleCartPoleEnv(gym.Env):
 
     def __init__(self, timeStep=0.1):
         # Boundaries
-        self.maxX = 3
+        self.maxX = 600
         self.maxG = 0.8
         self.maxT = 1
 
@@ -57,22 +57,25 @@ class DoubleCartPoleEnv(gym.Env):
                             dtype=np.float32)
         self._observation_space = spaces.Box(-boundary,
                                              boundary, dtype=np.float32)
-        
+
         self.reset()
         self.viewer = None
 
     def step(self, action):
         F = (2.0*action - self.coefR*self.mTot*self.g/2.0)/self.radW
 
-        _a = (-1 * F - self.mP * 0.5 * self.lenP * (self.state.p_dG ** 2) *
-              np.sin(self.state.p_G)) / self.mTot
-        _b = (4/3 - self.mP * (np.cos(self.state.p_g) ** 2) / self.mTot)
+        sinG = np.sin(self.state.p_G)
+        cosG = np.cos(self.state.p_G)
 
-        p_ddG = (self.g * np.sin(self.state.p_G) + np.cos(self.state.p_G) * _a) \
+        _a = (-1 * F - self.mP * 0.5 * self.lenP * (self.state.p_dG ** 2) *
+              sinG) / self.mTot
+        _b = (4/3 - self.mP * (cosG ** 2) / self.mTot)
+
+        p_ddG = (self.g * sinG + cosG * _a) \
             / (0.5 * self.lenP * _b)
 
-        _c = (self.state.p_dG ** 2) * np.sin(self.state.p_G) - \
-            p_ddG * np.cos(self.state.p_G)
+        _c = (self.state.p_dG ** 2) * sinG - \
+            p_ddG * cosG
         c_ddX = (F + self.mP * 0.5 * self.lenP * _c) / self.mTot
 
         self.state.c_dX += self.dt * c_ddX
@@ -87,12 +90,84 @@ class DoubleCartPoleEnv(gym.Env):
         return dt_replace(self.state), action, .0, terminate
 
     def reset(self):
-        self.state = State(p_dG=0.1)
+        self.state = State(p_dG=0.01)
 
     def render(self, mode='human'):
-        pass
+        screen_w = 600
+        screen_h = 400
+
+        world_width = self.maxX * 1.5
+        scale = screen_width/world_width
+        car_top = 100  # TOP OF CART
+        pole_w = 10.0
+        polelen = scale * self.lenP
+        car_w = 60.0
+        car_h = 30.0
+        wheel_r = 4.0
+
+        if self.viewer is None:
+            from gym.envs.classic_control import rendering
+            self.viewer = rendering.Viewer(screen_w, screen_h)
+
+            # Wheel geometry
+            wheel = rendering.make_circle(wheel_r)
+            self.wheeltrans_l = rendering.Transform(
+                translation=(-1/3*car_w, wheel_r))
+            wheel.set_color(0.7, 0, 0)
+            wheel.add_attr(self.wheeltrans_l)
+            self.viewer.add_geom(wheel)
+            wheel = rendering.make_circle(wheel_r)
+            self.wheeltrans_r = rendering.Transform(
+                translation=(1/3*car_w, wheel_r))
+            wheel.set_color(0.7, 0, 0)
+            wheel.add_attr(self.wheeltrans_r)
+            self.viewer.add_geom(wheel)
+
+            # Car geometry
+            l, r, t, b = -car_w / 2, car_w / 2, car_h / 2, -car_h / 2
+            car = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            self.cartrans = rendering.Transform(translation=(0, wheel_r))
+            car.add_attr(self.cartrans)
+            self.viewer.add_geom(car)
+
+            # Pole geometry
+            l, r, t, b = -pole_w / 2, pole_w / 2, polelen, 0
+            pole = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            pole.set_color(.7, .6, .3)
+            self.poletrans = rendering.Transform(translation=(0, car_h))
+            pole.add_attr(self.poletrans)
+            pole.add_attr(self.cartrans)
+            self.viewer.add_geom(pole)
+
+            # Axle
+            axle = rendering.make_circle(pole_w/2)
+            axle.add_attr(self.poletrans)
+            axle.add_attr(self.cartrans)
+            axle.set_color(.5, .5, .8)
+            self.viewer.add_geom(axle)
+
+            # Ground
+            ground = rendering.Line((0, car_top), (screen_w, car_top))
+            ground.set_color(0, 0, 0)
+            self.viewer.add_geom(ground)
+
+            self._pole_geom = pole
+
+        if self.state is None:
+            return None
+
+        pole = self._pole_geom
+        l, r, t, b = -pole_w / 2, pole_w / 2, polelen, 0
+        pole.v = [(l, b), (l, t), (r, t), (r, b)]
+
+        s = self.state
+        carX = s.c_X * scale + screen_w / 2 # middle of cart
+        self.wheeltrans_l.set_translation(carX - 1/3 * car_w, wheel_r)
+        self.wheeltrans_r.set_translation(carX + 1/3 * car_w, wheel_r)
+        self.cartrans.set_translation(carX, car_top + wheel_r)
+        self.poletrans.set_rotation(-s.p_G)
 
     def close(self):
         if self.viewer:
-          self.viewer.close()
-          self.viewer = None
+            self.viewer.close()
+            self.viewer = None
