564dafc8d2ff7772507d0ebdf2c5251499acd928
Kovács Gergely <kovika98@sch.bme.hu>
2021-05-04 17:45:54 +0200

    render works

--- src/doubleCartPoleEnv.py
+++ src/doubleCartPoleEnv.py
@@ -26,14 +26,14 @@ class DoubleCartPoleEnv(gym.Env):
 
     def __init__(self, timeStep=0.1):
         # Boundaries
-        self.maxX = 600
-        self.maxG = 0.8
+        self.maxX = 2.4
+        self.maxG = 12 * 2 * np.pi / 360
         self.maxT = 1
 
         # Cart data
         self.mC = 1
         self.mP = 0.1
-        self.lenP = 0.8
+        self.lenP = 1
         self.radW = 0.2
         self.friction = 1
         self.coefR = 0.001
@@ -47,9 +47,9 @@ class DoubleCartPoleEnv(gym.Env):
         self.dt = timeStep
         # spaces.Box(-self.maxT, self.maxT, shape=(1,))
         self._action_space = spaces.Discrete(7)
-        boundary = np.array([self.maxG*2,
+        boundary = np.array([self.maxG,
                              np.finfo(np.float32).max,
-                             self.maxX*2,
+                             self.maxX,
                              np.finfo(np.float32).max],
                             dtype=np.float32)
         self._observation_space = spaces.Box(-boundary,
@@ -58,9 +58,9 @@ class DoubleCartPoleEnv(gym.Env):
         self.viewer = None
 
     def step(self, action):
-        tourque = np.linspace(-self.maxT, self.maxT,
+        torque = np.linspace(-self.maxT, self.maxT,
                               self.action_space.n)[action]
-        F = (2.0*tourque - self.coefR*self.mTot*self.g/2.0)/self.radW
+        F = (2.0*torque - self.coefR*self.mTot*self.g/2.0)/self.radW
 
         sinG = np.sin(self.state.p_G)
         cosG = np.cos(self.state.p_G)
@@ -95,44 +95,44 @@ class DoubleCartPoleEnv(gym.Env):
         screen_w = 600
         screen_h = 400
 
-        world_width = self.maxX * 1.5
+        world_width = self.maxX * 2
         scale = screen_w/world_width
-        car_top = 100  # TOP OF CART
-        pole_w = 10.0
+        pole_w = 8.0
         polelen = scale * self.lenP
-        car_w = 60.0
-        car_h = 30.0
-        wheel_r = 4.0
+        car_w = 70.0
+        car_h = 40.0
+        wheel_r = 8.0
 
         if self.viewer is None:
             from gym.envs.classic_control import rendering
             self.viewer = rendering.Viewer(screen_w, screen_h)
 
-            """ # Wheel geometry
+            # Wheel geometry
             wheel = rendering.make_circle(wheel_r)
             self.wheeltrans_l = rendering.Transform(
                 translation=(-1/3*car_w, wheel_r))
-            wheel.set_color(0.7, 0, 0)
+            wheel.set_color(.7, .7, .7)
             wheel.add_attr(self.wheeltrans_l)
             self.viewer.add_geom(wheel)
             wheel = rendering.make_circle(wheel_r)
             self.wheeltrans_r = rendering.Transform(
                 translation=(1/3*car_w, wheel_r))
-            wheel.set_color(0.7, 0, 0)
+            wheel.set_color(.7, .7, .7)
             wheel.add_attr(self.wheeltrans_r)
             self.viewer.add_geom(wheel)
 
             # Car geometry
-            l, r, t, b = -car_w / 2, car_w / 2, car_h / 2, -car_h / 2
+            l, r, t, b = -car_w / 2, car_w / 2, car_h, 0
             car = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
             self.cartrans = rendering.Transform(translation=(0, wheel_r))
             car.add_attr(self.cartrans)
+            car.set_color(0.3, 0.3, 1)
             self.viewer.add_geom(car)
 
             # Pole geometry
             l, r, t, b = -pole_w / 2, pole_w / 2, polelen, 0
             pole = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
-            pole.set_color(.7, .6, .3)
+            pole.set_color(0, 0.9, 0.4)
             self.poletrans = rendering.Transform(translation=(0, car_h))
             pole.add_attr(self.poletrans)
             pole.add_attr(self.cartrans)
@@ -142,20 +142,15 @@ class DoubleCartPoleEnv(gym.Env):
             axle = rendering.make_circle(pole_w/2)
             axle.add_attr(self.poletrans)
             axle.add_attr(self.cartrans)
-            axle.set_color(.5, .5, .8)
-            self.viewer.add_geom(axle) """
+            axle.set_color(.45, .45, .45)
+            self.viewer.add_geom(axle)
 
-            # Ground
-            ground = rendering.Line((0, car_top), (screen_w, car_top))
-            ground.set_color(0, 0, 0)
-            self.viewer.add_geom(ground)
-
-            #self._pole_geom = pole
+            self._pole_geom = pole
 
         if self.state is None:
             return None
 
-        """ pole = self._pole_geom
+        pole = self._pole_geom
         l, r, t, b = -pole_w / 2, pole_w / 2, polelen, 0
         pole.v = [(l, b), (l, t), (r, t), (r, b)]
 
@@ -163,10 +158,17 @@ class DoubleCartPoleEnv(gym.Env):
         carX = s.c_X * scale + screen_w / 2  # middle of cart
         self.wheeltrans_l.set_translation(carX - 1/3 * car_w, wheel_r)
         self.wheeltrans_r.set_translation(carX + 1/3 * car_w, wheel_r)
-        self.cartrans.set_translation(carX, car_top + wheel_r)
-        self.poletrans.set_rotation(-s.p_G) """
+        self.cartrans.set_translation(carX, wheel_r)
+        self.poletrans.set_rotation(-s.p_G)
+
+        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
 
     def close(self):
         if self.viewer:
             self.viewer.close()
             self.viewer = None
+
+env=DoubleCartPoleEnv()
+env.reset()
+env.render()
+input("")
--- src/run.py
+++ src/run.py
@@ -2,12 +2,14 @@ from doubleCartPoleEnv import DoubleCartPoleEnv
 import agent as ag
 import gym
 
+
 def registerEnv():
     gym.envs.register(
-     id='DoubleCartPoleEnv-v0',
-     entry_point='gym.envs.classic_control:DoubleCartPoleEnv',
-     max_episode_steps=1000,
-)
+        id='DoubleCartPoleEnv-v0',
+        entry_point='gym.envs.classic_control:DoubleCartPoleEnv',
+        max_episode_steps=1000,
+    )
+
 
 def run():
     env = DoubleCartPoleEnv()
@@ -16,9 +18,9 @@ def run():
     rewards_history = agent.train(env)
     print("Finished training, testing...")
     for i in range(200):
-        print("%d out of 200" % agent.test(env,True))
-    
+        print("%d out of 200" % agent.test(env, True))
+
 
 if __name__ == "__main__":
-    #registerEnv()
+    # registerEnv()
     run()
