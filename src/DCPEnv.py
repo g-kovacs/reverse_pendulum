7e17a9d5873ff9ad67ba1e10f001c273ff6c579f
kovika98 <61557355+kovika98@users.noreply.github.com>
2021-05-06 21:32:23 +0200

    made new renderer

--- src/DCPEnv.py
+++ src/DCPEnv.py
@@ -2,6 +2,7 @@ import gym
 from gym import spaces
 from dataclasses import dataclass, replace as dt_replace
 import numpy as np
+from CarRenderer import CarRenderer
 
 
 class DCPEnv(gym.Env):
@@ -22,12 +23,12 @@ class DCPEnv(gym.Env):
     dt = 0.1
 
     # Computed values
-    mC = car_width + 2 * (radW ** 2 ) * np.pi
+    mC = car_width + 2 * (radW ** 2) * np.pi
     mP = lenP * 0.2
     p_I = 1/3 * mP * (lenP ** 2)
     mTot = mC + mP
 
-    @dataclass
+    @dataclass(order=False)
     class State:
         p_G: float = 0.0
         p_dG: float = 0.0
@@ -41,7 +42,8 @@ class DCPEnv(gym.Env):
             self.p_dG += torque / DCPEnv.p_I * DCPEnv.dt
 
         def add_torque(self, torque):
-            F = (2.0*torque - DCPEnv.coefR * DCPEnv.mTot * DCPEnv.g/2.0)/DCPEnv.radW
+            F = (2.0*torque - DCPEnv.coefR *
+                 DCPEnv.mTot * DCPEnv.g/2.0)/DCPEnv.radW
 
             sinG = np.sin(self.p_G)
             cosG = np.cos(self.p_G)
@@ -83,13 +85,20 @@ class DCPEnv(gym.Env):
                                              boundary, dtype=np.float32)
         DCPEnv.dt = timeStep
         self.viewer = None
+        self.render_data = {"wW": self.maxX * 2, "pW": self.mP / self.lenP,
+                            "pL": self.lenP, "cW": self.car_width, "wR": self.radW}
 
+    def _init_renderer(self):
+        self.viewer = CarRenderer(data_dict=self.render_data)
+        self.viewer.add_car(CarRenderer.Colors.BLUE)
+    
     def step(self, action):
         torque = np.linspace(-self.maxT, self.maxT,
                              self.action_space.n)[action]
 
         if np.random.random() < 1e-3:
-            self.state.wind_blow(np.random.choice([-self.maxT * 10, self.maxT * 10]))
+            self.state.wind_blow(np.random.choice(
+                [-self.maxT * 10, self.maxT * 10]))
 
         self.state.add_torque(torque)
 
@@ -99,7 +108,9 @@ class DCPEnv(gym.Env):
         return np.array(dt_replace(self.state).flatten()), 1.0, terminate, {"action": action}
 
     def reset(self):
-        self.state = DCPEnv.State(p_dG=0.01)
+        if self.viewer is None:
+            self._init_renderer()
+        self.state = DCPEnv.State(p_dG=0.01, c_X=2)
         return np.array(dt_replace(self.state).flatten())
 
     def test(self, model, render=True):
@@ -107,82 +118,24 @@ class DCPEnv(gym.Env):
         while not done:
             if render:
                 self.render()
-            action, _ = model.action_value(obs[None, :])
+            action, _ = model.action_value(obs[None, :], False)
             obs, reward, done, _ = self.step(action)
             ep_reward += reward
         return ep_reward
 
     def render(self, mode='human'):
-        screen_w = 600
-        screen_h = 400
-
-        world_width = self.maxX * 2
-        scale = screen_w/world_width
-        pole_w = scale * self.mP / self.lenP
-        polelen = scale * self.lenP
-        car_w = scale * self.car_width
-        car_h = car_w / 2
-        wheel_r = scale * self.radW
-
         if self.viewer is None:
-            from gym.envs.classic_control import rendering
-            self.viewer = rendering.Viewer(screen_w, screen_h)
-
-            # Car geometry
-            l, r, t, b = -car_w / 2, car_w / 2, car_h, 0
-            car = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
-            self.cartrans = rendering.Transform(translation=(0, wheel_r))
-            car.add_attr(self.cartrans)
-            car.set_color(0.3, 0.3, 1)
-            self.viewer.add_geom(car)
-
-            # Wheel geometry
-            wheel = rendering.make_circle(wheel_r)
-            self.wheeltrans_l = rendering.Transform(
-                translation=(-1/3*car_w, wheel_r))
-            wheel.set_color(.6, .6, .6)
-            wheel.add_attr(self.wheeltrans_l)
-            self.viewer.add_geom(wheel)
-            wheel = rendering.make_circle(wheel_r)
-            self.wheeltrans_r = rendering.Transform(
-                translation=(1/3*car_w, wheel_r))
-            wheel.set_color(.6, .6, .6)
-            wheel.add_attr(self.wheeltrans_r)
-            self.viewer.add_geom(wheel)
-
-            # Pole geometry
-            l, r, t, b = -pole_w / 2, pole_w / 2, polelen, 0
-            pole = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
-            pole.set_color(0, 0.9, 0.4)
-            self.poletrans = rendering.Transform(translation=(0, car_h))
-            pole.add_attr(self.poletrans)
-            pole.add_attr(self.cartrans)
-            self.viewer.add_geom(pole)
-
-            # Axle
-            axle = rendering.make_circle(pole_w/2)
-            axle.add_attr(self.poletrans)
-            axle.add_attr(self.cartrans)
-            axle.set_color(.45, .45, .45)
-            self.viewer.add_geom(axle)
-
-            self._pole_geom = pole
+            return None
+            #self._pole_geom = pole
 
         if self.state is None:
             return None
 
-        pole = self._pole_geom
-        l, r, t, b = -pole_w / 2, pole_w / 2, polelen, 0
-        pole.v = [(l, b), (l, t), (r, t), (r, b)]
+        # pole = self._pole_geom
+        # l, r, t, b = -pole_w / 2, pole_w / 2, polelen, 0
+        # pole.v = [(l, b), (l, t), (r, t), (r, b)]
 
-        s = self.state
-        carX = s.c_X * scale + screen_w / 2  # middle of cart
-        self.wheeltrans_l.set_translation(carX - 1/3 * car_w, wheel_r)
-        self.wheeltrans_r.set_translation(carX + 1/3 * car_w, wheel_r)
-        self.cartrans.set_translation(carX, wheel_r)
-        self.poletrans.set_rotation(-s.p_G)
-
-        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
+        return self.viewer.render_cars([self.state])
 
     def close(self):
         if self.viewer:
--- src/eval.py
+++ src/eval.py
@@ -5,7 +5,7 @@ def main():
     env = DCPEnv()
     model = SimpleModel(num_actions=env.action_space.n)
     model.load_weights('saves/simpleModel_1.0').expect_partial()
-    for i in range(4):
+    for i in range(30):
         print("Alive for %.1f seconds" % (env.test(model, True) / 10.0))
     env.close()
 
