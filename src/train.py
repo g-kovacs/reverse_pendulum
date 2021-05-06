0ab3280527f90f5c6174b6c9dbdb4d80a7549b69
kovika98 <61557355+kovika98@users.noreply.github.com>
2021-05-06 17:35:16 +0200

    rm save

--- src/train.py
+++ src/train.py
@@ -13,7 +13,7 @@ def run():
     plt.plot(rewards_history)
     plt.show()
     print("Finished training, testing...")
-    print(f'Test result: {env.test(model, False)}')
+    print(f'Test result: {env.test(model, False) / 10.0}')
     model.save_weights('saves/simpleModel_1.0')
 
 if __name__ == "__main__":
