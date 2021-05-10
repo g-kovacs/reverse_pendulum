a6c8af8e4e66848f2cab20f53eb854ed0238d6fe
kovika98 <61557355+kovika98@users.noreply.github.com>
2021-05-10 15:34:15 +0200

    srcs formatted

--- src/agent.py
+++ src/agent.py
@@ -3,6 +3,7 @@ import tensorflow.keras.losses as kls
 import tensorflow.keras.optimizers as ko
 import numpy as np
 
+
 class A2CAgent:
     def __init__(self, lr=7e-3, gamma=0.99, value_c=0.5, entropy_c=1e-4):
         # `gamma` is the discount factor
@@ -22,12 +23,14 @@ class A2CAgent:
 
         # Sparse categorical CE loss obj that supports sample_weight arg on `call()`.
         # `from_logits` argument ensures transformation into normalized probabilities.
-        weighted_sparse_ce = kls.SparseCategoricalCrossentropy(from_logits=True)
+        weighted_sparse_ce = kls.SparseCategoricalCrossentropy(
+            from_logits=True)
 
         # Policy loss is defined by policy gradients, weighted by advantages.
         # Note: we only calculate the loss on the actions we've actually taken.
         actions = tf.cast(actions, tf.int32)
-        policy_loss = weighted_sparse_ce(actions, logits, sample_weight=advantages)
+        policy_loss = weighted_sparse_ce(
+            actions, logits, sample_weight=advantages)
 
         # Entropy loss can be calculated as cross-entropy over itself.
         probs = tf.nn.softmax(logits)
@@ -37,16 +40,17 @@ class A2CAgent:
         # Here signs are flipped because the optimizer minimizes.
         return policy_loss - self.entropy_c * entropy_loss
 
-    def train(self, env, config, batch_size=128, updates=500, max_step = 300):
+    def train(self, env, config, batch_size=128, updates=500, max_step=300):
         models = config.get()
         for model in models:
             model.compile(optimizer=ko.RMSprop(lr=self.lr),
-                    loss=[self._logits_loss, self._value_loss])
+                          loss=[self._logits_loss, self._value_loss])
         model_num = len(models)
         # Storage helpers for a single batch of data.
         actions = np.empty((batch_size, config.num), dtype=np.int32)
         rewards, dones, values = np.empty((3, batch_size, config.num))
-        observations = np.empty((batch_size, config.window_size, env.observations_size))
+        observations = np.empty(
+            (batch_size, config.window_size, env.observations_size))
 
         # Training loop: collect samples, send to optimizer, repeat updates times.
         deaths = {}
@@ -60,8 +64,10 @@ class A2CAgent:
                 steps += 1
                 observations[step] = obs_window
                 for m_i, model in enumerate(models):
-                    actions[step, m_i], values[step, m_i] = model.action_value(obs_window)
-                obs_window, rewards[step], dones[step] = env.step(actions[step])
+                    actions[step, m_i], values[step,
+                                               m_i] = model.action_value(obs_window)
+                obs_window, rewards[step], dones[step] = env.step(
+                    actions[step])
                 if any(dones[step]) or max_step < steps:
                     obs_window = env.reset()
                     episodes.append(steps*env.dt)
@@ -71,14 +77,16 @@ class A2CAgent:
                             deaths[model.label] += 1
             for m_i, model in enumerate(models):
                 _, next_value = model.action_value(obs_window)
-                returns, advs = self._returns_advantages(rewards[:,m_i],
-                                                        dones[:,m_i],
-                                                        values[:,m_i],
-                                                        next_value)
+                returns, advs = self._returns_advantages(rewards[:, m_i],
+                                                         dones[:, m_i],
+                                                         values[:, m_i],
+                                                         next_value)
                 # A trick to input actions and advantages through same API.
-                acts_and_advs = np.concatenate([actions[:,m_i, None], advs[:, None]], axis=-1)
-                model.train_on_batch(observations[:,-model.input_size:,:], [acts_and_advs, returns])
-        return episodes, deaths    
+                acts_and_advs = np.concatenate(
+                    [actions[:, m_i, None], advs[:, None]], axis=-1)
+                model.train_on_batch(
+                    observations[:, -model.input_size:, :], [acts_and_advs, returns])
+        return episodes, deaths
 
     def _returns_advantages(self, rewards, dones, values, next_value):
         # `next_value` is the bootstrap value estimate of the future state (critic).
@@ -86,7 +94,8 @@ class A2CAgent:
 
         # Returns are calculated as discounted sum of future rewards.
         for t in reversed(range(len(rewards))):
-            returns[t] = rewards[t] + self.gamma * returns[t + 1] * (1 - dones[t])
+            returns[t] = rewards[t] + self.gamma * \
+                returns[t + 1] * (1 - dones[t])
         returns = returns[:-1]
 
         # Advantages are equal to returns - baseline (value estimates in our case).
--- src/eval.py
+++ src/eval.py
@@ -1,6 +1,7 @@
 from DCPEnv import DCPEnv
 from Models import ModelConfiguration
 
+
 def main():
     config = ModelConfiguration.load('AC2vsLSTM')
     env = DCPEnv(num_cars=config.num, buffer_size=config.window_size)
@@ -8,6 +9,7 @@ def main():
         print("Alive for %.1f seconds" % (env.test(config.get())[0] / 10.0))
     env.close()
     config.clean()
+
 
 if __name__ == "__main__":
     main()
