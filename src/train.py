7bf3d2f1eb4eab7cbeb91c78fb6ca01b40cb021e
kovika98 <61557355+kovika98@users.noreply.github.com>
2021-05-10 15:31:03 +0200

    format code

--- src/train.py
+++ src/train.py
@@ -14,34 +14,36 @@ train.py usage
     -g:     Use GPU for calculations (default is CPU)
 """
 batch_update = (512, 300)
-cfg_name = '.'.join(['AC2vsLSTM', 'b'+str(batch_update[0]), 'u'+str(batch_update[1])])
+cfg_name = '.'.join(
+    ['AC2vsLSTM', 'b'+str(batch_update[0]), 'u'+str(batch_update[1])])
+
 
 def run():
-    config=Models.ModelConfiguration([
+    config = Models.ModelConfiguration([
         # Models.SimpleAC(num_actions=DCPEnv.actions_size),
         Models.SimpleAC2(num_actions=DCPEnv.actions_size),
         Models.LSTMModel(num_actions=DCPEnv.actions_size),
     ], cfg_name)
-    env=DCPEnv(num_cars=config.num, buffer_size=config.window_size)
-    agent=A2CAgent()
-    starttime=timer()
-    episodes, deaths=agent.train(env, config, *batch_update)
+    env = DCPEnv(num_cars=config.num, buffer_size=config.window_size)
+    agent = A2CAgent()
+    starttime = timer()
+    episodes, deaths = agent.train(env, config, *batch_update)
     config.save()
-    dt=timer() - starttime
+    dt = timer() - starttime
 
-    fig=plt.figure()
+    fig = plt.figure()
     if(len(deaths) > 1):
-        spec=fig.add_gridspec(ncols=2, nrows=1, width_ratios=[1, 4])
-        ax=fig.add_subplot(spec[0])
+        spec = fig.add_gridspec(ncols=2, nrows=1, width_ratios=[1, 4])
+        ax = fig.add_subplot(spec[0])
         ax.set_title("Death Counts")
         ax.pie(list(deaths.values()),
-                    labels=list(deaths.keys()),
-                    explode=[0.1]*config.num,
-                    shadow=True,
-                    autopct=lambda p: f'{int(p * sum(deaths.values())/100)}')
-        ax=fig.add_subplot(spec[1])
+               labels=list(deaths.keys()),
+               explode=[0.1]*config.num,
+               shadow=True,
+               autopct=lambda p: f'{int(p * sum(deaths.values())/100)}')
+        ax = fig.add_subplot(spec[1])
     else:
-        ax=fig.add_subplot(111)
+        ax = fig.add_subplot(111)
     ax.set_title("Training history")
     ax.plot(episodes, 'bo', markersize=2)
     ax.set(xlabel='episodes', ylabel='seconds')
@@ -49,8 +51,8 @@ def run():
     plt.draw()
     print(f"Finished training in {int(dt+1)} seconds, testing...")
     if not os.path.exists('media'):
-            os.makedirs('media')
-    seconds, death_list=env.test(
+        os.makedirs('media')
+    seconds, death_list = env.test(
         config.get(), True, f'media/{config.label}.gif')
     print(f'Alive for {int(seconds)} seconds')
     print('Died:')
@@ -61,12 +63,12 @@ def run():
 
 
 def main(argv):
-    mpl.rcParams['toolbar']='None'
+    mpl.rcParams['toolbar'] = 'None'
 
-    os.environ['CUDA_VISIBLE_DEVICES']='-1'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
     try:
-        opts, args=getopt.getopt(argv, 'hg')
+        opts, args = getopt.getopt(argv, 'hg')
     except getopt.GetoptError:
         print(helpMSG)
         sys.exit(2)
